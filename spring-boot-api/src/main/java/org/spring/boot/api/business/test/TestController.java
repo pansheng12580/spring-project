864806a261004f8b930c10b2fc98dc0e7f89c903
pantheon <542452226@qq.com>
2016-07-26 16:02:01 +0800

    spring-boot-api集成h2database，开启h2 console

--- spring-boot-api/src/main/java/org/spring/boot/api/business/test/TestController.java
+++ spring-boot-api/src/main/java/org/spring/boot/api/business/test/TestController.java
@@ -1,7 +1,5 @@
 package org.spring.boot.api.business.test;
 
-import java.util.Properties;
-
 import org.spring.boot.api.base.BaseRequestBean;
 import org.spring.boot.api.util.LogUtil;
 import org.springframework.web.bind.annotation.RequestMapping;
